b98c7c5f65efa0c58deb7faf7af35ce90ec5fb62
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2019-11-19 07:19:40 +0000

    Subclasses of XYChart

---
+++ src/main/java/zh/learn/javafx/ch23charts/xycharts/LineChartTest.java
@@ -0,0 +1,15 @@
+package zh.learn.javafx.ch23charts.xycharts;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LineChartTest extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+    }
+}
